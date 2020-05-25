7c4fb6e239406804c9a50bdac7ca3d259c390fe3
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-05-25 14:57:06 +0200

    cars controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cars/index'
+  get 'cars/show'
+  get 'cars/new'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cars, only: [:index, :show]
 end
